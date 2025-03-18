16ec0a767729486362d87513165565c159c5a330
chandan.b <cyancolony@gmail.com>
2025-03-18 17:41:40 +0530

    added files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "radical-aws-infra-tfstate-dev"
+    bucket = "radical-aws-infra-tfstate-dev-12345"
     key    = "terraform.tfstate"
     region = "ap-south-1"
   }
